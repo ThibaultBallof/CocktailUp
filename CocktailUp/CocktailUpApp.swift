130e222fe4b26b0df6571fb9a09d9a794c4b3d3b
Thibault Ballof <thibault.ballof@worldline.com>
2023-06-06 18:32:32 +0200

    Initial Commit

---
+++ CocktailUp/CocktailUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailUpApp.swift
+//  CocktailUp
+//
+//  Created by Thibault Ballof on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CocktailUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
